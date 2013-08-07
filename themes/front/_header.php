4edf88e8a2ea1e6bde66b9b6905d9e1279dadd7b
unknown <DeVo@.(none)>
2013-08-07 15:48:13 +0700

    logout , check login

--- themes/front/_header.php
+++ themes/front/_header.php
@@ -4,7 +4,7 @@
 		<h4>คณะสาธารณะสุขศาสตร์ มหาวิทยาลัยมหิดล</h4>
 		<?php if(is_login()):?>
 		<span>เข้าสู่ระบบโดย <a href="setting/user_form/<?php echo login_data('id')?>" class="link_login"><?php echo login_data('name')?> </a> </span>
-		| <a href="admin/logout">ออกจากระบบ</a>
+		| <a href="home/signout">ออกจากระบบ</a>
 		<?php // echo login_data('USER_TYPE_ID')?>
 		<?php endif;?>
 	</div>		
