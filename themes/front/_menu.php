cee5d2a9997f54e117ef3f5bc40ae4bfa572550d
unknown <Teerawat@.(none)>
2013-08-15 14:36:57 +0700

    commit est

--- themes/front/_menu.php
+++ themes/front/_menu.php
@@ -1,11 +1,11 @@
 <div class="light_blue">  
 <ul id="mega-menu-6" class="mega-menu unstyled">
 <? 
-$admin_system = $this->db->getarray("SELECT * FROM admin_menu where pid = 0 ORDER BY order_no ");	
+$admin_system = $this->db->getarray("SELECT * FROM admin_menu where pid = 0 and show_menu = 1 ORDER BY order_no ");	
 foreach($admin_system as $system):
 	$url = $system['url']!='' ? $system['url'] : '#';
 	echo '<li><a href="'.$url.'">'.$system['title']."</a>";
-	$admin_menu = $this->db->getarray("SELECT * FROM admin_menu WHERE pid=".$system['id']." order by order_no ");
+	$admin_menu = $this->db->getarray("SELECT * FROM admin_menu WHERE pid=".$system['id']." and show_menu = 1 order by order_no ");
 	if(count($admin_menu) >0)echo '<ul>';
 	foreach($admin_menu as $item):	
 		 if(permission($item['id'],'canview')){
